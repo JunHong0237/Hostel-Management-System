6085292595b7ae9a2d90ce3a9a4a848bd1af08a0
JunHong0237 <154218489+JunHong0237@users.noreply.github.com>
2024-03-27 09:38:19 +0000

    db

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ const __dirname = dirname(__filename);
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 db.connect((err) => {
